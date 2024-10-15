1dc4153482a010ce2374881f01a9f98c93c0a70e
Ali <adadashipour@gmail.com>
2024-10-15 14:02:04 +0100

    Add main code to fetch joke

---
+++ JokeFetcher/Dtos/Joke.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokeFetcher.Dtos
+{
+    public class Joke
+    {
+        public string Setup { get; set; } = string.Empty;
+        public string Punchline {  get; set; } = string.Empty;
+    }
+}
--- JokeFetcher/Program.cs
+++ JokeFetcher/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Set the base URL for the Web Sevice (API)
+using JokeFetcher.Dtos;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+const string baseUrl = "https://official-joke-api.appspot.com/";
+
+//Create custom options for JsonSerialiser to ignore case when deserialising
+var options = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true,
+};
+
+var client = new HttpClient { BaseAddress = new Uri(baseUrl) };
+
+//Send a GET request to the "random_joke" endpoint
+HttpResponseMessage response = client.GetAsync("jokes/random").Result;
+
+//Ensure the request was successful (throws an exception if unsuccessful) 
+response.EnsureSuccessStatusCode();
+
+//Read the response content as a string 
+string responseBody = response.Content.ReadAsStringAsync().Result;
+
+//Deserialise the JSON response into a joke object (ignore case on key names)
+var joke = JsonSerializer.Deserialize<Joke>(responseBody, options);
+
+//Check if the joke is null
+if (joke != null )
+{
+    //Display the joke in the console
+    Console.WriteLine("Here's a joke for you.");
+    Console.WriteLine(joke.Setup);
+    Console.WriteLine(joke.Punchline);
+} else
+{
+    Console.WriteLine("Failed to fetch a joke.");
+}
+
